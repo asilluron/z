6f70dbda8e8fdc9c3a2c1d1a15499bd09c1e03e7
Andrew Silluron <asilluron@users.noreply.github.com>
2015-05-10 17:08:35 -0700

    switch to UMD. closes #6

--- z.js
+++ z.js
@@ -1,3 +1,11 @@
-module.exports = function(){
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.Z = factory();
+  }
+}(this, function () {
     return "z";
-};
+}));
